ee426e44df0ff518bfdab544fbe0c22a3b617d99
yangding233 <150337404+yangding233@users.noreply.github.com>
2024-01-20 10:06:19 +0800

    Add files via upload

---
+++ first_class.c
@@ -0,0 +1,54 @@
+//============================第一个程序=========================
+// #include<stdio.h>//standard input output
+
+// #define PRICE 10
+// int main()//主函数：程序的入口//只能有一个主函数
+// {
+//     int num;//int 是整形的意思、、main前面的int表示main函数效用返回一个整形值
+//     int totalvalue;
+//     num = 10;
+//     totalvalue = num * PRICE;
+//     printf("totalvalue = %d\n", totalvalue);
+//     return 0;
+// }
+// ctrl+/  快速注释
+//============================第二个程序==============================
+// #include <stdio.h>
+// int main()
+// {
+//     char ch = 'A';//char:字符数字类型
+//     printf("%c\n",ch);//%c--打印字符格式的数据//%f--打印浮点数字—答应小数
+//     return 0;
+// }
+// =============================第三个程序===========================
+// #include <stdio.h>
+
+// int main()
+// {
+//     int age = 20;//short int long long_long:短整型，整形，长整形，更长的整形
+//     printf("%d\n",age);//%d--打印整形十进制数据
+//     return 0;
+// }
+//=========================第四个程序=====================
+// #include <stdio.h>
+// int num1 = 100;//全局变量
+// int main()
+// {
+//     int num1 = 10;//局部变量//全局变量与局部变量的名字尽量不要相同
+//     printf("%d\n",num1);                  //若相同，则局部变量优先
+//     return 0;
+// }
+//==========================第五个程序===========================
+// #include<stdio.h>
+//计算两个数的和
+// int main()
+// {
+//     int num1 = 0;
+//     int num2 = 0;
+//     int sum = 0;//c语言语法规定，变量要定义在当前代码块的最前面
+//     scanf("%d%d",&num1,&num2);//取地址符号&
+//     sum = num1 + num2;
+//     printf("sum = %d\n",sum);
+//     return 0;
+// }
+
